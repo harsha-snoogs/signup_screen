4d11b05653fbaa70b4d9a5d5b6ac66666469db8d
harsha-snoogs <shv.harshan68@gmail.com>
2025-12-18 19:14:32 +0530

    Chg: name import change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
 import Signup from './components/signup/Signup'
-import Main from './components/main/main.jsx'
+import Main from './components/main/Main.jsx'
 function App() {
 
   return (
     <div className="screen">
-      <div className="main"><Main/></div>
+      <div className="main"><Main /></div>
       <div className="aside"><Signup /></div>
     </div>
   )
